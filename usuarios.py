4c63a45985781325c656d5f173a88db8dc2b0263
charlesdev771 <charles077211@gmail.com>
2023-01-20 20:53:38 -0300

    Final 1.0

--- usuarios.py
+++ usuarios.py
@@ -68,39 +68,3 @@ def comments():
 
 
 
-
-@bp_usuarios.route('/recovery')
-def recovery():
-    usuarios = Usuario.query.all()
-    return render_template('usuarios_recovery.html', usuarios=usuarios)
-
-
-@bp_usuarios.route('/update/<int:id>', methods=['GET', 'POST'])
-def update(id):
-    u = Usuario.query.get(id)
-
-    if request.method == 'GET':
-        return render_template('usuarios_update.html', u=u)
-
-    if request.method == 'POST':
-        nome = request.form.get('nome')
-        email = request.form.get('email')
-        u.nome = nome
-        u.email = email
-        db.session.add(u)
-        db.session.commit()
-        return redirect(
-            'http://127.0.0.1:5000/usuarios/recovery')
-
-
-@bp_usuarios.route('/delete/<int:id>', methods=['GET', 'POST'])
-def delete(id):
-    u = Usuario.query.get(id)
-    if request.method == 'GET':
-        return render_template('usuarios_delete.html', u=u)
-
-    if request.method == 'POST':
-        db.session.delete(u)
-        db.session.commit()
-        return redirect(
-            'http://127.0.0.1:5000/usuarios/recovery')
